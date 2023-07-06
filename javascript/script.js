98076fe42f1840dc9c3f4bd6f9fc063c949d361f
amruta2425 <amruta.marathe25@gmail.com>
2023-07-06 14:26:33 +1000

    added files

--- javascript/script.js
+++ javascript/script.js
@@ -1,38 +1,34 @@
 import {reverseMorseCodeDict ,translateToMorseCode, translateToEnglish } from "./translate.js";
 
-// Event listener for translate button
+
 const translateBtn = document.getElementById('translate-btn');
 translateBtn.addEventListener('click', () => {
-  const inputText = document.getElementById('text-input').value;
-  const resultTextArea = document.getElementById('translated-text');
-  if (inputText.startsWith('.') || inputText.startsWith('-')) {
+  try {
+    const inputText = document.getElementById('text-input').value;
+    const resultTextArea = document.getElementById('translated-text');
 
-    // Morse Code to English translation
-
-    if (inputText.match(/[^.\s-]/)) {
-
-      // Alert for invalid characters in Morse code input
-      alert("Invalid input! Please enter valid Morse Code without alphabetic characters or special characters.");
-    } 
-    else {
-      const translatedText = translateToEnglish(inputText);
-      resultTextArea.value = translatedText;
+    if (inputText.startsWith('.') || inputText.startsWith('-')) {
+      // Morse Code to English translation
+      if (inputText.match(/[^.\s-]/)) {
+        throw new Error("Invalid input! Please enter valid Morse Code without alphabetic characters or special characters.");
+      } else {
+        const translatedText = translateToEnglish(inputText);
+        resultTextArea.value = translatedText;
+      }
+    } else {
+      // English to Morse Code translation
+      if (inputText.match(/[0-9@$%^&*()_+={}|[\]\\:;"'<>,.?/#-]/)) {
+        throw new Error("Invalid input! Please enter valid English text without special characters or numbers.");
+      } else {
+        const translatedText = translateToMorseCode(inputText);
+        resultTextArea.value = translatedText;
+      }
     }
-  } else {
-
-    // English to Morse Code translation
-    if (inputText.match(/[0-9@$%^&*()_+={}|[\]\\:;"'<>,.?/#-]/)) {
-
-      // Alert for invalid characters in English input
-      alert("Invalid input! Please enter valid English text without special characters or numbers.");
-    } 
-    else {
-      
-      const translatedText = translateToMorseCode(inputText);
-      resultTextArea.value = translatedText;
-    }
+  } catch (error) {
+    alert(error.message);
   }
 });
+
 
 // Clear button click event
 
